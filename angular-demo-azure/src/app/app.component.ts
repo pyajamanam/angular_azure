f1976553fb72f7aaba65e0b626ead2d6f1898462
Pradeep_Yazamanam <Pradeep_Yazamanam@EPINHYDW0B31>
2022-08-02 14:53:58 +0530

    view child

--- angular-demo-azure/src/app/app.component.ts
+++ angular-demo-azure/src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ContentChild, ElementRef, Input, ViewChild } from '@angular/core';
+import { Hero, HEROES } from './data/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit{
+  ngAfterViewInit(): void {
+   
+  }
   title = 'angular-demo-azure';
  loggeduserparent = "Pradeeps" ;
-ctop:string = '';
- 
-
+ ctop:string = '';
+ @Input() loading = false;
 addItem(newItem: string) {
 this.ctop= newItem;
 }
+
+onSelect(hero:Hero)
+{
+  console.log(hero);
+}
+@Input()
+heroes = HEROES;
+selectedHero?: Hero;
+testfromparent:string="testfromparent";
 }
